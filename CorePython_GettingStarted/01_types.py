5ae28a79d69866e1e6561a146574fce90dc4105b
Michael Beesley <mrbeesley@users.noreply.github.com>
2021-09-23 14:37:06 -0500

    working through the module on scalar types

---
+++ CorePython_GettingStarted/01_types.py
@@ -0,0 +1,26 @@
+# Scalar Types
+
+#int - are signed and have unlimited precision based on memory of machine
+x = 10
+print(x)
+x = 0b10
+print(x)
+x = 0o10
+print(x)
+x = 0x10
+print(x)
+x = int(3.5)
+print(x)
+x = int(-3.5)
+print(x)
+x = int("496")
+print(x)
+
+
+#float
+
+
+#null
+
+
+#bool
