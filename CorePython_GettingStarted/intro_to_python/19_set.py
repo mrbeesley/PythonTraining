08f36175d38493e3d02c2665f789649234b53117
Michael Beesley <mrbeesley@users.noreply.github.com>
2021-10-18 09:39:57 -0500

    finished sets

--- CorePython_GettingStarted/intro_to_python/19_set.py
+++ CorePython_GettingStarted/intro_to_python/19_set.py
@@ -2,3 +2,69 @@
 p = {6, 28, 496, 8128, 33550336}
 print(f'p = {p}')
 print(type(p))
+e = set()
+
+# a common use for a set is to remove duplicate items from a list
+s = set([2, 4, 16, 64, 4096, 65536, 4, 262144, 16])
+print('s = set([2, 4, 16, 64, 4096, 65536, 4, 262144, 16])')
+print(f's = {s}')
+t = [1, 4, 2, 1, 7, 9, 9]
+print(f't = {t}')
+print(f'set(t) = {set(t)}')
+
+print('Iterate a set')
+print('for x in s:')
+for x in s:
+    print(f'    {x}')
+
+print(f'1 in s: {1 in s}')
+print(f'3 in s: {3 in s}')
+print(f'1 not in s: {1 not in s}')
+
+k = {81, 108}
+print(f'k = {k}')
+k.add(54)
+k.add(12)
+k.add(108)
+k.update([37, 128, 54, 97])
+print('k.add(54)')
+print('k.add(12)')
+print('k.update([37, 128, 54, 97])')
+print(f'k = {k}')
+
+k.remove(97)
+# discard doesn't though a key error if it can't find the item to remove
+k.discard(98)
+k.discard(37)
+print('k.remove(97)')
+print('k.discard(98)')
+print('k.discard(37)')
+print(f'k = {k}')
+
+# shallow copy works the same as other collections
+j = k.copy()
+
+# set algebra
+blue_eyes = {'olivia', 'harry', 'lily', 'jack', 'amelia'}
+blond_hair = {'harry', 'jack', 'amelia', 'mia', 'joshua'}
+smell_hcn = {'harry', 'amelia'}
+taste_ptc = {'harry', 'lily', 'amelia', 'lola'}
+o_blood = {'mia', 'joshua', 'lily', 'olivia'}
+b_blood = {'amelia', 'jack'}
+a_blood = {'harry'}
+ab_blood = {'joshua', 'lola'}
+print(f'blue_eyes = {blue_eyes}')
+print(f'blond_hair = {blond_hair}')
+print(f'smell_hcn = {smell_hcn}')
+print(f'taste_ptc = {taste_ptc}')
+print(f'o_blood = {o_blood}')
+print(f'b_blood = {b_blood}')
+print(f'a_blood = {a_blood}')
+print(f'ab_blood = {ab_blood}')
+print(f'Bond Hair or Blue Eyes: {blue_eyes.union(blond_hair)}')
+print(f'blue_eyes.union(blond_hair) == blond_hair.union(blue_eyes: {blue_eyes.union(blond_hair) == blond_hair.union(blue_eyes)}')
+print(f'Bond Hair and Blue Eyes: {blue_eyes.intersection(blond_hair)}')
+print(f'Bond Hair and not Blue Eyes: {blond_hair.difference(blue_eyes)}')
+print(f"smell_hcn.issubset(blond_hair): {smell_hcn.issubset(blond_hair)}")
+print(f"taste_ptc.issuperset(smell_hcn): {taste_ptc.issuperset(smell_hcn)}")
+print(f'a_blood.isdisjoint(o_blood): {a_blood.isdisjoint(o_blood)}')
--- CorePython_GettingStarted/notes/course_notes.py
+++ CorePython_GettingStarted/notes/course_notes.py
@@ -15,11 +15,6 @@ def course_notes():
     """
 
     print('\n\n')
-    objects_and_types()
-    string_collections()
-
-
-def objects_and_types():
     variable_management()
     print('\n\n')
     value_equality()
@@ -29,6 +24,20 @@ def objects_and_types():
     default_value_evaluation()
     print('\n\n')
     scopes_in_python
+    print('\n\n')
+    string_collections()
+    print('\n\n')
+    collection_protocols()
+    print('\n\n')
+    tuples_in_python()
+    print('\n\n')
+    ranges_in_python()
+    print('\n\n')
+    lists_in_python()
+    print('\n\n')
+    dictionaries_in_python()
+    print('\n\n')
+    sets_in_python()
     print('\n\n')
 
 
@@ -42,48 +51,104 @@ def variable_management():
 
 def value_equality():
     print('=================================Value vs Identity Equality=================================')
-    print('Value-Equality and identity equality are fundamentally different concepts')    
-    print('comparison by value can be controlled programatically')
-    print('identity comparison is unalterably defined by the language')
+    print(' 1. Value-Equality and identity equality are fundamentally different concepts')    
+    print(' 2. comparison by value can be controlled programatically')
+    print(' 3. identity comparison is unalterably defined by the language')
 
 
 def passing_arguments():
     print('=================================Passing Arguments=================================')
-    print('when you pass and argument it is passed by creating a new label against the underlying object')
-    print('for example if you pass a list and then modify the list it is modified in both scopes')
-    print('it is the responsibility of the function to copy the object if that is the desired result')
-    print('function arguments are transferred using pass-by-object-reference')
-    print('references to objects are copoied, not the objects themselves')
-    print('Arguments with default values must come after those without default values')
+    print(' 1. when you pass and argument it is passed by creating a new label against the underlying object')
+    print(' 2. for example if you pass a list and then modify the list it is modified in both scopes')
+    print(' 3. it is the responsibility of the function to copy the object if that is the desired result')
+    print(' 4. function arguments are transferred using pass-by-object-reference')
+    print(' 5. references to objects are copoied, not the objects themselves')
+    print(' 6. Arguments with default values must come after those without default values')
 
 
 def default_value_evaluation():
     print('=================================Default Value Evaluation=================================')
-    print('remember that def is a statemnt executed at runtime')
-    print('default arguments are evaluated when def is executed')
-    print('immutable default values dont cause problems')
-    print('mutable default values can cause confusing effects since they are only evaluted once')
-    print('RULE: Always use immutable objects for default values')
+    print(' 1. remember that def is a statemnt executed at runtime')
+    print(' 2. default arguments are evaluated when def is executed')
+    print(' 3. immutable default values dont cause problems')
+    print(' 4. mutable default values can cause confusing effects since they are only evaluted once')
+    print(' 5. RULE: Always use immutable objects for default values')
 
 
 def scopes_in_python():
     print('=================================Scopes in Python=================================')
-    print('LEGB')
-    print('LOCAL: inside the current function')
-    print('ENCLOSING: inside the enclosing functions')
-    print('GLOBAL: at the top level of the module')
-    print('BUILT-IN: in the special builtins module')
+    print(' LEGB')
+    print('     LOCAL: inside the current function')
+    print('     ENCLOSING: inside the enclosing functions')
+    print('     GLOBAL: at the top level of the module')
+    print('     BUILT-IN: in the special builtins module')
     print('*note - use the global keyword to explicitely reference a top level module variable')
 
 
 def string_collections():
     print('=================================String Collections in Python=================================')
-    print('1. Concatenation with + results in temporaries')
-    print('2. Use str.join() to join strings, to prevent high memory usage')
-    print('3. str.join() inserts a separator between a collection of strings')
-    print('4. call join() on the separator string')
+    print(' 1. Concatenation with + results in temporaries')
+    print(' 2. Use str.join() to join strings, to prevent high memory usage')
+    print(' 3. str.join() inserts a separator between a collection of strings')
+    print(' 4. call join() on the separator string')
+    print(' 5. use str.join() for efficient string concatenation')
+    print(' 6. use str.partition() for certain simple string parsing operations')
+    print(' 7. str.format() is a powerful string interpolation technique')
+    print(' 8. f-strings are a kind of string literal for performing interpolation')
     print('* to concatenate invoke join on empty text, something from nothing')
 
+
+def collection_protocols():
+    print('=================================Protocols in Python=================================')
+    print(' 1. A set of operations that a type must suppor to implement the protocol')
+    print(' 2. do not need to be defined as interfaces or base classes')
+    print(' 3. types only need to provide functioning implementations')
+
+
+def tuples_in_python():
+    print('=================================Tuples in Python=================================')
+    print(' 1. tuples are immutable sequences')
+    print(' 2. tuple literals are optional parentheses around comma seperated items')
+    print(' 3. use a trailing comma for a single-element tuples')
+    print(' 4. tuple unpacking is useful for multiple return values and swapping')
+
+
+def ranges_in_python():
+    print('=================================Ranges in Python=================================')
+    print(' 1. range objects represetn arithmetic progressions of integers')
+    print(' 2. range() can be called with one two or three arguments: start stop and step')
+    print(' 3. enumerate is often better than range for making loop counters')
+
+
+def lists_in_python():
+    print('=================================Lists in Python=================================')
+    print(' 1. list supports indexing from the end with negative indices')
+    print(' 2. slicing copies all or part of a list')
+    print(' 3. the full slice is a common idiom for copying lists')
+    print(' 4. use list.index() and list.count() to look for elements in a list')
+    print(' 5. use the del keyword to remove elements from a list')
+    print(' 6. sort and reverse lists in-place with list.sort() and list.reverse()')
+    print(' 7. sorted() and reversed() sort and reverse any iterable')
+    print(' 8. list copies are shallow, only copying the references')
+
+
+def dictionaries_in_python():
+    print('=================================Dictionaries in Python=================================')
+    print(' 1. dictionaries map from keys to values')
+    print(' 2. iteration and membership in dictionaries are over keys')
+    print(' 3. do not assume any order when iterating dictionary keys')
+    print(' 4. dict.keys(), dict.values() and dict.items() are iterable views into dictionaries')
+    print(' 5. copy dictionaries with dict.copy() or the dict constructor')
+    print(' 6. use dict.update() to extend one dictionary with another')
+
+
+def sets_in_python():
+    print('=================================Sets in Python=================================')
+    print(' 1. sets are unordered collections of unique elements')
+    print(' 2. sets support powerful set-algebra operations and predicates')
+    print(' 3. buile-in collections can be organized by protocols')
+    print(' 4. underscore often represents unused values')
+    print(' 5. the pprint module supports pretty printing of compound data structures')
 
 
 if __name__ == '__main__':
